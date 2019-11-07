0163248254f02b64c7959773fa50d3c4e12a7c5b
FernandoRivas26 <n4nd0_ro@hotmail.com>
2019-11-07 07:42:58 +0000

    Table donadores after query |:D

--- php/getDonadores.php
+++ php/getDonadores.php
@@ -1,7 +1,7 @@
 <?php
     include('conexion.php');
-    $salida="";
-    $sql = "SELECT id, CONCAT(nombres,' ',a_paterno,' ', a_materno) AS nombre, correo, telefono, tipos_sangre.tipo, resp_nombre, resp_tel, carreras.nombre AS 'carrera', datosutd.cuatri, 'Predonante' AS 'efectivo' FROM donadores INNER JOIN tipos_sangre INNER JOIN datosutd INNER JOIN carreras ON (donadores.id=datosutd.id_donadores AND datosutd.carrera=carreras.id_carrera AND donadores.tipo_sangre = tipos_sangre.id_tipo_sangre ) WHERE donadores.id NOT IN (SELECT id_donadores FROM donaciones) UNION SELECT id, CONCAT(nombres,' ',a_paterno,' ', a_materno) AS nombre, correo, telefono, tipos_sangre.tipo, resp_nombre, resp_tel, carreras.nombre AS 'carrera', datosutd.cuatri, 'Efectivo' AS 'efectivo' FROM donadores INNER JOIN tipos_sangre INNER JOIN datosutd INNER JOIN carreras ON (donadores.id=datosutd.id_donadores AND datosutd.carrera=carreras.id_carrera AND donadores.tipo_sangre = tipos_sangre.id_tipo_sangre ) WHERE donadores.id IN (SELECT id_donadores FROM donaciones)";
+
+    $sql = "SELECT CONCAT(donadores.nombres,' ', donadores.a_paterno,' ', donadores.a_materno) AS nombre, donadores.correo, donadores.telefono, tipos_sangre.tipo, donadores.resp_nombre, donadores.resp_tel FROM donadores INNER JOIN donaciones INNER JOIN tipos_sangre ON (donadores.id = donaciones.id_donadores AND donadores.tipo_sangre = tipos_sangre.id_tipo_sangre)";
 
     $result = mysqli_query($con, $sql);
     $datos = array();
